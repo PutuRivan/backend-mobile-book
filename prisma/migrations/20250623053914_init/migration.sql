216bae9084fb0bcdb8580873f8f4882e8654d889
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-06-23 12:39:49 +0700

    migrate

--- prisma/migrations/20250623053914_init/migration.sql
+++ prisma/migrations/20250623053914_init/migration.sql
@@ -1,23 +1,28 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
-  - You are about to drop the column `updateAt` on the `user` table. All the data in the column will be lost.
-  - You are about to drop the `cart` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-*/
--- DropForeignKey
-ALTER TABLE `cart` DROP FOREIGN KEY `cart_id_book_fkey`;
+-- CreateTable
+CREATE TABLE `Book` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `authors` VARCHAR(191) NULL,
+    `thumbnail` TEXT NULL,
+    `description` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
 
--- DropForeignKey
-ALTER TABLE `cart` DROP FOREIGN KEY `cart_id_user_fkey`;
-
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `updateAt`,
-    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
-
--- DropTable
-DROP TABLE `cart`;
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Review` (
@@ -53,6 +58,3 @@ ALTER TABLE `Favorite` ADD CONSTRAINT `Favorite_userId_fkey` FOREIGN KEY (`userI
 
 -- AddForeignKey
 ALTER TABLE `Favorite` ADD CONSTRAINT `Favorite_bookId_fkey` FOREIGN KEY (`bookId`) REFERENCES `Book`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- RenameIndex
-ALTER TABLE `user` RENAME INDEX `user_email_key` TO `User_email_key`;
